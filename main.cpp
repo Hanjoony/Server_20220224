4f0c98bb231a778e26b4ad92eb101324d12237af
Hanjoony <sh00go@naver.com>
2022-02-24 16:43:01 +0900

    구조체를 이용한 전송

--- main.cpp
+++ main.cpp
@@ -2,26 +2,38 @@
 
 #include <WinSock2.h>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-#pragma comment(lib, "ws2_32.lib")		// 윈도우에 기본으로 깔려있는 라이브러리 불러오기 필요
+#pragma comment(lib, "ws2_32.lib")
+
+
+
+struct NumberPacket {
+	int Number1;
+	int Number2;
+};
 
 int main()
 {
 	WSAData wsaData;
 
-	// 1. winsock 초기화, 윈도우만 합니다. linux, unix 에선 사용하지 않음
+	//1.winsock 초기화, 윈도우만 합니다. linux, unix에선 사용하지 않아요.
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData))
 	{
-		cout << "Winsock Error" << GetLastError() << endl;		// GetLastError() 마지막에 난 에러의 번호를 리턴함
-		exit(-1);												// 여기까지가 winsock 초기화
+		cout << "Winsock Error : " << GetLastError() << endl;
+		exit(-1);
 	}
 
-	// 2. create socket
-	SOCKET ServerSocket;										//listen 클라이언트 대기 소켓(문)
+	//2.create socket
+	SOCKET ServerSocket;//listen 클라이언트 대기 소켓(문)
 
-	ServerSocket = socket(AF_INET, SOCK_STREAM, 0);
+	ServerSocket = socket(
+		AF_INET,
+		SOCK_STREAM,
+		0
+	);
 
 	if (ServerSocket == INVALID_SOCKET)
 	{
@@ -29,60 +41,67 @@ int main()
 		exit(-1);
 	}
 
-	// 2. ip, port를 할당해서 소켓이랑 연결
+	//2. ip, port를 할당해서 소켓이랑 연결
 	SOCKADDR_IN ServerAddr;
 
-	memset(&ServerAddr, 0, sizeof(ServerAddr));			// 메모리 영역을 0으로 채움
+	memset(&ServerAddr, 0, sizeof(ServerAddr));
+	ServerAddr.sin_family = AF_INET;
+	ServerAddr.sin_port = htons(50000);
+	ServerAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	ServerAddr.sin_family = AF_INET;					// 2바이트
-	ServerAddr.sin_port = htons(50000);					// 2바이트
-	ServerAddr.sin_addr.s_addr = htonl(INADDR_ANY);		// 4바이트
-	
-	if (bind(ServerSocket, (SOCKADDR*)&ServerAddr, sizeof(ServerAddr)) == SOCKET_ERROR)		//  소켓 에러 확인, port가 겹쳐서 에러나는 경우 많음
+	if (bind(ServerSocket, (SOCKADDR*)&ServerAddr, sizeof(ServerAddr)) == SOCKET_ERROR)
 	{
 		cout << "bind Error : " << GetLastError() << endl;
 		exit(-1);
 	}
 
-	// 3. 클라이언트 연결을 기다려라
+	//3. 클라이언트 연결을 기다려라.
 	if (listen(ServerSocket, 0) == SOCKET_ERROR)
 	{
 		cout << "listen Error : " << GetLastError() << endl;
 		exit(-1);
 	}
-	while (1)				// 서버가 안 꺼지게 반복
+
+	while (1)
 	{
-		// 4. 수락해서 클라이언트와 연결을 완성
+		//4. 수락해서 클라이언트와 연결을 완성
 		SOCKADDR_IN ClientAddr;
-		int ClientAddrSize = sizeof(ClientAddr);
-		SOCKET ClientSocket = 0;					// 클라이언트가 된 통로 (문)
-		ClientSocket = accept(ServerSocket, (SOCKADDR*)&ClientAddr, &ClientAddrSize);
+		int ClinetAddrSize = sizeof(ClientAddr);
+		SOCKET ClientSocket; //클라이언트 된 통로(문)
+		ClientSocket = accept(ServerSocket, (SOCKADDR*)&ClientAddr, &ClinetAddrSize);
 		if (ClientSocket == SOCKET_ERROR)
 		{
 			cout << "accept Error : " << GetLastError() << endl;
 			exit(-1);
 		}
 
-		cout << "connect ip : " << inet_ntoa(ClientAddr.sin_addr) << endl;		// inet network to address
-		cout << "connect port : " << ntohs(ClientAddr.sin_port) << endl; 		// network to host short
+		//inet network to address
+		cout << "connect ip : " << inet_ntoa(ClientAddr.sin_addr) << endl;
+		//network to host short
+		cout << "connect port : " << ntohs(ClientAddr.sin_port) << endl;
 
-		// 5. 보낸다 자료를 클라이언트한테
-		char Message[1024] = { 0, };
-		recv(ClientSocket, Message, 1024 - 1, 0);
+		//5. 보낸다 자료를 클라이언트한테
+		NumberPacket Packet;
+		recv(ClientSocket, (char*)(&Packet), 8, 0);
 
-		cout << "client sended : " << Message << endl;
+		cout << "client sended : " << Packet.Number1 << endl;
+		cout << "client sended : " << Packet.Number2 << endl;
 
-		send(ClientSocket, Message, strlen(Message) + 1, 0);
+		int IntResult = Packet.Number1 + Packet.Number2;
 
-		cout << "client send : " << Message << endl;
+		string Result = to_string(IntResult);
 
-		// 6. 연결 종료
+		send(ClientSocket, Result.c_str(), Result.length() + 1, 0);
+
+		cout << "client send : " << Result << endl;
+
+		//6. 연결 종료
 		closesocket(ClientSocket);
 	}
 
 	closesocket(ServerSocket);
 
-	// 7. Winsock 종료,	1번하고 7번은 윈도우만 함
+	//7.Winsock 종료, 윈도우만 합니다.
 	WSACleanup();
 
 	return 0;
